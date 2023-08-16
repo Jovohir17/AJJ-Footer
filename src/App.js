172a1cf442774b9e5addd961627d973006c63224
joha17 <johax9@gmail.com>
2023-08-16 17:42:56 +0300

    footer project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
+
+class App extends Component {
+    render() {
+        return (
+            <>
+                <Main/>
+                <Footer/>
+            </>
+        );
+    }
+}
+
+export default App;
